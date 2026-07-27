5fce0f8cb641b961bb326bd0c9e2ca0427a18b9f
YogitaBhikajiPtil <patilyogita0303@gmail.com>
2026-07-27 14:24:13 +0530

    fix login api url for deployment

--- public/login.js
+++ public/login.js
@@ -13,7 +13,7 @@ form.addEventListener("submit", async function (e) {
   }
 
   try {
-    const res = await axios.post("http://localhost:3000/auth/login", {
+    const res = await axios.post("/auth/login", {
       email,
       password
     });
